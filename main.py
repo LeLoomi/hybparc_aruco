2056541f6762efb7826044826d748b12fdd354cf
Eliah Lohr <lohr.eliah@gmail.com>
2024-05-16 14:25:22 +0200

    Ported processing widget from original ros project

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton, QVBoxLayout, QLineEdit, QWidget, QSpacerItem
 from login_widget import LoginWidget
 from place_electrodes_widget import PlaceElectrodesWidget
+from processing_widget import ProcessingWidget
 
 class MainWindow(QMainWindow):
     
     # Entry into the GUI
-    def __init__(self) -> None:
+    def __init__(self):
+        print('[Hybparc] Booting up')
         super().__init__()
         # Window setup
         self.showMaximized()
@@ -13,12 +15,13 @@ class MainWindow(QMainWindow):
         self.show_login_widget()
 
     def set_login_info(self, login_name, login_domain):
-        print('login with {}@{}'.format(login_name, login_domain))
+        print('[Hybparc] Logged in as {}@{}'.format(login_name, login_domain))
         self.login_name = login_name
         self.login_domain = login_domain
 
     # First displayed widget
     def show_login_widget(self):
+        print('[Hybparc] Diplaying login widget')
         login_widget = LoginWidget()
         login_widget.loginSuccessful.connect(self.set_login_info)
         login_widget.loginSuccessful.connect(self.show_place_electrodes)
@@ -26,9 +29,21 @@ class MainWindow(QMainWindow):
 
     # Displayed after successful login
     def show_place_electrodes(self):
+        print('[Hybparc] Diplaying electrode placement widget')
         place_electrodes_widget = PlaceElectrodesWidget()
+        place_electrodes_widget.electrodes_placed.connect(self.show_processing_widget)
         self.setCentralWidget(place_electrodes_widget)
+    
+    def show_processing_widget(self):
+        print('[Hybparc] Displaying processing widget')
+        processing_widget = ProcessingWidget()
+        processing_widget.processing_complete.connect(self.show_results_widget)
+        self.setCentralWidget(processing_widget)
 
+    def show_results_widget(self):
+        print('[Hybparc] Displaying results widget')
+        pass
+    
 if __name__ == "__main__":
     app = QApplication([])
     window = MainWindow()
---
+++ processing_widget.py
@@ -0,0 +1,73 @@
+from PyQt6.QtWidgets import QWidget, QDialogButtonBox, QHBoxLayout, QLabel, QPushButton, QVBoxLayout
+from PyQt6.QtGui import QPixmap, QMovie
+from PyQt6.QtCore import Qt, pyqtSignal, QSize, QTimer
+
+#include <QTimer>
+
+class ProcessingWidget(QWidget):
+    
+    processing_complete = pyqtSignal()
+    
+    def __init__(self):
+        super().__init__()
+    
+        # Setup ui
+        self.iconLabel = QLabel()
+        pixmap = QPixmap('./icons/correct.svg')
+        scaledPixmap = pixmap.scaled(QSize(250, 200))
+        self.iconLabel.setPixmap(scaledPixmap)
+        self.iconLabel.setVisible(False)
+
+        self.textLabel = QLabel('Berechne Daten. Bitte trete vom Körper zurück <br> und stelle sicher, dass die Marker nicht verdeckt sind.')
+        self.textLabel.setTextFormat(Qt.TextFormat.RichText)
+
+        font = self.textLabel.font()
+        font.setPointSize(32)
+        self.textLabel.setFont(font)
+
+        self.gifLabel = QLabel()
+        self.movie = QMovie('./gifs/animation.gif')
+        self.movie.setScaledSize(QSize(200, 200))
+        self.gifLabel.setMovie(self.movie)
+        self.gifLabel.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        iconTextLayout = QHBoxLayout()
+        iconTextLayout.addStretch()
+        iconTextLayout.addWidget(self.iconLabel)
+        iconTextLayout.addWidget(self.textLabel)
+        iconTextLayout.addStretch()
+
+        self.buttonBox = QDialogButtonBox()
+        okButton = self.buttonBox.addButton('Ok', QDialogButtonBox.ButtonRole.AcceptRole)
+        okButton.setFont(font)
+        self.buttonBox.setVisible(False)
+
+        contentLayout = QVBoxLayout()
+        contentLayout.addStretch()
+        contentLayout.addLayout(iconTextLayout)
+        contentLayout.addSpacing(50)
+        contentLayout.addWidget(self.gifLabel)
+        contentLayout.addWidget(self.buttonBox)
+        contentLayout.addStretch()
+
+        mainLayout = QHBoxLayout()
+        mainLayout.addStretch()
+        mainLayout.addLayout(contentLayout)
+        mainLayout.addStretch()
+
+        self.setLayout(mainLayout)
+        self.movie.start()
+
+        isFirstTry = False
+        QTimer.singleShot(5000 if isFirstTry else 2000, lambda: self.handle_timeout(
+            self.iconLabel, self.textLabel, self.gifLabel, self.movie, self.buttonBox))
+
+    # We go here after fake processing time
+    def handle_timeout(self, iconLabel, textLabel, gifLabel, movie, buttonBox):
+        iconLabel.setVisible(True)
+        movie.stop()
+        gifLabel.setVisible(False)
+        textLabel.setText('Auswertung erfolgreich! <br> Klicke auf Ok, um fortzufahren.')
+        buttonBox.setVisible(True)
+        
+        self.processing_complete.emit()
