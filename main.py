37c12b9fc3728d6befc9d99fd96e7b9efab34673
Eliah Lohr <lohr.eliah@gmail.com>
2024-10-17 11:36:00 +0200

    Transitioned away from imutils towards bare cv (might be needed might not be needed, depends on HP PolyLens); added SETUP.md as a guide. to be expanded

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
+import cv2 as cv    # sadly we have to load cv here, since we use it to capture the images
 from ArucoRoi.detector import Detector
-from cv2 import imread
-from numpy import concatenate
-from imutils.video import VideoStream
 from PyQt6.QtWidgets import QApplication, QMainWindow
 from login_widget import LoginWidget
 from place_electrodes_widget import PlaceElectrodesWidget
 from processing_widget import ProcessingWidget
 from results_widget import ResultsWidget
-from qt_material import apply_stylesheet
+from qt_material import apply_stylesheet    # optional prettifier
 
 class MainWindow(QMainWindow):
     
@@ -16,7 +14,7 @@ class MainWindow(QMainWindow):
         print('[Hybparc] Booting up')
         super().__init__()
         
-        # Detector setup
+        # Detector setup, loaded file is the ROI config
         self.detector = Detector('./config.json')
         
         # Window setup
@@ -44,30 +42,32 @@ class MainWindow(QMainWindow):
         place_electrodes_widget.electrodes_placed.connect(self.show_processing_widget)
         self.setCentralWidget(place_electrodes_widget)
         
-        # Warm up cameras
-        self.stream0 = VideoStream(0).start()
-        self.stream1 = VideoStream(1).start()
+        # Setup and warm up cameras
+        self.stream0 = cv.VideoCapture(index=0, apiPreference=cv.CAP_DSHOW)
+        self.stream1 = cv.VideoCapture(index=1, apiPreference=cv.CAP_DSHOW)
     
     def show_processing_widget(self):
         print('[Hybparc] Displaying processing widget')
         
-        # show ui, disappears after time by its own (fake loading screen)
+        # show ui, disappears after time by its own (fake loading screen to give cams time)
         processing_widget = ProcessingWidget()
         processing_widget.processing_complete.connect(self.show_results_widget)
         self.setCentralWidget(processing_widget)
         
         # actually run detection
-        in0 = self.stream0.read()
-        in1 = self.stream1.read()
-        frame = concatenate((in0, in1), axis=0)
+        ret0, in0 = self.stream0.read()
+        ret1, in1 = self.stream1.read()
+        
+        frame = cv.vconcat(in0, in1)
         result_frame, self.roi_statuses = self.detector.image_detect(frame) # ! DETECTION CALL
 
     def show_results_widget(self):
         print('[Hybparc] Displaying results widget')
         results_widget = ResultsWidget(self.roi_statuses)
-        #results_widget.retry_triggered.connect(self.show_place_electrodes)
+        # TODO results_widget.retry_triggered.connect(self.show_place_electrodes)
         self.setCentralWidget(results_widget)
-        self.stream.stop()
+        self.stream0.release()
+        self.stream1.release()
     
 if __name__ == "__main__":
     app = QApplication([])
